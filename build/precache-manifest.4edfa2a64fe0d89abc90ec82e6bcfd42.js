7f8b40ecf2b822731a43180e88ca1740890d083f
sofiapenttila <50035212+sofiapenttila@users.noreply.github.com>
2023-04-04 21:24:31 -0700

    Add files via upload

---
+++ build/precache-manifest.4edfa2a64fe0d89abc90ec82e6bcfd42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891949eb42ecfd7052a7649c07e1f451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "043fa5ccfb65af3f9f10",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "485df8937ebd7af109f3",
+    "url": "/static/js/2.3a10e04a.chunk.js"
+  },
+  {
+    "revision": "7dc2529ea9c355027961799bc1ffe0c3",
+    "url": "/static/js/2.3a10e04a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043fa5ccfb65af3f9f10",
+    "url": "/static/js/main.0c45846c.chunk.js"
+  },
+  {
+    "revision": "fd4a854c06bb36be3a7d",
+    "url": "/static/js/runtime-main.1530841d.js"
+  }
+]);
